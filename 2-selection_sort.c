cd2706ca3927cb52e3f3a6500c7087b31369e908
mbmande <mbsmande@gmail.com>
2024-02-23 07:42:23 -0800

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * swaping - =======
+ * @m: =======
+ * @n: =====
+ *
+ */
+
+void swaping(int *m, int *n)
+{
+        int t;
+
+        t = *m;
+        *m = *n;
+        *n = t;
+}
+
+/**
+ * selection_sort - ======
+ * @array: ======
+ * @size: ====
+ * Description: =====
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	int *in;
+	size_t l, o;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (l = 0; l < size - 1; l++)
+	{
+		in = array + l;
+		for (o = l + 1; o < size; o++)
+			in = (array[o] < *in) ? (array + o) : in;
+
+		if ((array + l) != in)
+		{
+			swaping(array + l, in);
+			print_array(array, size);
+		}
+	}
+}
